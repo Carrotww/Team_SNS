b622987e607bf60273e0b5500de91e11e2898b28
Beam-ki <skunn123@gmail.com>
2022-09-30 03:05:36 +0900

    css 다수 추가 및 setting,urls 수정 프로필페이지 작성

--- present_sns/present_sns/settings.py
+++ present_sns/present_sns/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static'), )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
